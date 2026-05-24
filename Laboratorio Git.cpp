e3251df913af0b6921a5931a1991bdfed991a56a
jchavezl-hub <jchavezl@unal.edu.co>
2026-05-24 12:03:22 -0500

    agregué código para leer el archivo

--- Laboratorio Git.cpp
+++ Laboratorio Git.cpp
@@ -1,5 +1,16 @@
-#include<iostream>
+#include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
-int main(){
-	
+
+int main() {
+    ifstream archivoEntrada("input/ArchivoEntrada.txt");
+    if (archivoEntrada.is_open()) {
+        cout << "Archivo abierto" << endl;
+        string linea;
+        while (getline(archivoEntrada, linea)) {
+            cout << linea << endl;
+        }
+    }
+    return 0;
 }
